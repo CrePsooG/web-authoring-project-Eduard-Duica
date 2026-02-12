a4028a6f697a1d22d70a1bb2ae6587670432aa66
CrePsooG <71946084+CrePsooG@users.noreply.github.com>
2026-02-12 17:56:15 +0000

    Made nav bar responsive and added some colors to the pages.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Open modal
 const openModal = document.querySelectorAll(".open-btn");
 const closeModal = document.querySelectorAll(".close-btn");
 
@@ -11,4 +12,17 @@ closeModal.forEach(element => {
     element.addEventListener("click", () => {
         element.parentElement.close();
     })
+})
+
+// open sidebar mobile
+const sidebar = document.querySelector(".sidebar");
+const openSidebar = document.querySelector(".hamburger-icon")
+const closeSidebar = document.querySelector(".close-icon")
+
+openSidebar.addEventListener("click", ()=> {
+    sidebar.style.display = "flex";
+})
+
+closeSidebar.addEventListener("click", ()=> {
+    sidebar.style.display = "none";
 })
